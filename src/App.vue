33f81580827ab7d74d0a3fdf0e4024dbc2b209b7
David <david@Davids-iMac.local>
2023-02-12 09:49:29 +0000

    init commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="border-b-2 border-black">
+    <div class="flex justify-between mx-5 sm:mx-10  items-center h-24">
+      <RouterLink  class="text-2xl" to="/">Eola</RouterLink>
+      <nav>
+        <RouterLink to="/">Portfolio</RouterLink>
+        <RouterLink class="px-2 sm:px-10" to="/about">About</RouterLink>
+        <RouterLink  to="/contact">Contact</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
