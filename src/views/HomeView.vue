c11dfd565c9310a8a255d050bccd5e2dcd11eedb
David <david@Davids-iMac.local>
2023-03-05 22:44:32 +0000

    responsive home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
   <main>
     <section class="flex flex-col items-center mt-10 sm:flex-row flex-wrap max-w-screen-xl sm:mx-auto">
-      <div v-for="(image, index) in images" :key="index" class="wrapper w-full h-96 sm:w-6/12 md:w-3/12 md:mb-6">
+      <div v-for="(image, index) in images" :key="index" class="wrapper w-full h-96 sm:w-6/12 md:w-4/12 lg:w-3/12 md:mb-6">
         <div :class="[image, 'md:w-11/12 mx-auto bg-cover w-full h-full']">
-          <div class="link-cover bg-stone-900 bg-opacity-50 sm:bg-opacity-0 hover:bg-stone-900  hover:bg-opacity-50 bg-cover w-full md:w-11/12 group">
+          <div class="link-cover bg-stone-900 bg-opacity-50 lg:bg-opacity-0 hover:bg-stone-900  hover:bg-opacity-50 bg-cover w-full md:w-11/12 group">
             <RouterLink
-              class="flex sm:hidden text-2xl bg-opacity-50 w-full group-hover:flex justify-center uppercase text-slate-50 "
+              class="flex lg:hidden text-2xl bg-opacity-50 w-full group-hover:flex justify-center uppercase text-slate-50 "
               :to="'/project/' + (index + 1)"
               >Veiw Progect
             </RouterLink>
