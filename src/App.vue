fe126582fe17d6020c250bd78d430242f69f38b4
David <david@Davids-iMac.local>
2023-03-08 13:19:40 +0000

    lot

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="border-b-2 border-slate-500 bg-slate-50">
     <div class="flex justify-between mx-5 max-w-screen-xl px-3 sm:mx-auto items-center h-24">
-      <RouterLink  class="text-2xl" to="/">Eola</RouterLink>
+      <RouterLink  class="text-2xl font-bold" to="/">Charlotte</RouterLink>
       <nav>
         <RouterLink class="link" to="/">Portfolio</RouterLink>
         <RouterLink class="link px-2 sm:px-10" to="/about">About</RouterLink>
